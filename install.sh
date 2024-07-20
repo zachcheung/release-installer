a43f04dbaf96c619b83e5aff51644da31117b95a
Zach Cheung <kuroro.zhang@gmail.com>
2024-07-20 17:05:06 +0800

    install.sh: compare version first

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ type curl jq > /dev/null
 gh_owner=zachcheung
 gh_repo=release-installer
 install_dir=/usr/local/bin
+bin=$gh_repo
 
 kernel=$(uname -s)
 # Convert kernel name to lowercase
@@ -32,12 +33,21 @@ trap 'rm -rf "$temp_dir"' EXIT
 
 cd "$temp_dir"
 curl -fsSL https://api.github.com/repos/$gh_owner/$gh_repo/releases/latest \
-  | jq -r ".assets[] | select(.browser_download_url | test(\"${goos}_${goarch}\")) | .name + \" \" + .browser_download_url" \
-  | while IFS=' ' read -r name download_url; do
+  | jq -r ".tag_name as \$tag | .assets[] | select(.name | test(\"${goos}_${goarch}\")) | .name + \" \" + .browser_download_url + \" \" + \$tag" \
+  | while IFS=' ' read -r name download_url tag_name; do
+    if [ -x $install_dir/$bin ]; then
+      local_version=$($install_dir/$bin -version)
+      if [ "$local_version" = "$tag_name" ]; then
+        echo "$tag_name is the latest version, no need to upgrade"
+        exit 0
+      else
+        echo "upgrading from $local_version to $tag_name"
+      fi
+    fi
     echo "downloading $name"
     curl -fsSL -o "$name" "$download_url"
     echo "downloaded $name"
-    echo "installing $gh_repo"
+    echo "installing $bin"
     tar xf "$name"
     find . -type f -perm -u=x -exec mv -v {} $install_dir \;
   done
